7f367ed95e5c5460e3e2d1bd1ef7403aa3ead4d6
Tiago Longueira <tlongueira@cdt.com.ar>
2022-05-27 08:45:47 -0300

    first

---
+++ src/main/java/com/tlongueira/questionados/payload/response/MessageResponse.java
@@ -0,0 +1,18 @@
+package com.tlongueira.questionados.payload.response;
+
+public class MessageResponse {
+
+	private String message;
+
+	public MessageResponse(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
