4f1c35ed5f14274c363e2c1d91818dda0f28812d
Tiago Longueira <tlongueira@cdt.com.ar>
2022-06-02 23:45:57 -0300

    fix script select

--- src/main/java/com/tlongueira/questionados/repository/PreguntaRepository.java
+++ src/main/java/com/tlongueira/questionados/repository/PreguntaRepository.java
@@ -16,6 +16,6 @@ public interface PreguntaRepository extends JpaRepository<Pregunta, Long>{
 			 @Param("categoriaId") Long categoriaId
 			);
 	
-	@Query("SELECT p FROM preguntas p ORDER BY RANDOM() LIMIT 1")
+	@Query(nativeQuery = true,value="SELECT p FROM preguntas p ORDER BY RANDOM() LIMIT 1")
 	Pregunta getPreguntaRandom();
 }
